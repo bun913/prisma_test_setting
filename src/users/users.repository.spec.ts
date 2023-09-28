182d29f04c1632e43b0854e787811b11d8951695
bun913 <nagasima.romance+@gmail.com>
2023-09-28 09:47:00 +0900

    feat: jest-prismaの検証完了

--- src/users/users.repository.spec.ts
+++ src/users/users.repository.spec.ts
@@ -1,7 +1,8 @@
-import { getPrismaClient } from "../../dbClient";
+// import { getPrismaClient } from "../../dbClient";
+import { User } from "@prisma/client";
 import { UsersRepository } from "./users.repository";
 
-const userRepository = new UsersRepository(getPrismaClient());
+const userRepository = new UsersRepository(jestPrisma.client);
 
 describe("UsersRepository", () => {
   describe("createUser", () => {
@@ -10,8 +11,60 @@ describe("UsersRepository", () => {
         name: "test",
         email: "test@example.com",
       };
-      const result = await userRepository.createUser(userInput)
+      const result = await userRepository.createUser(userInput);
       expect(result.email).toEqual(userInput.email);
     });
+
+    it("should return a user2", async () => {
+      const userInput = {
+        name: "test",
+        email: "test@example.com",
+      };
+      const result = await userRepository.createUser(userInput);
+      expect(result.email).toEqual(userInput.email);
+    });
+  });
+
+  describe("updateUser", () => {
+    let testUser: User;
+
+    beforeEach(async () => {
+      testUser = await userRepository.createUser({
+        name: "test",
+        email: "test@example.com",
+      });
+    });
+
+    it("updates user name", async () => {
+      // arrange
+      const userInput = {
+        id: testUser.id,
+        name: "changed",
+      };
+
+      // act
+      const result = await userRepository.updateuser(userInput);
+
+      // assert
+      expect(result.name).toEqual(userInput.name);
+    });
+    
+    it("updates user name", async () => {
+      // この時点では当然変更されていない
+      expect(testUser.name).toEqual("test")
+      // arrange
+      const userInput = {
+        id: testUser.id,
+        name: "changed2",
+      };
+
+      // act
+      const result = await userRepository.updateuser(userInput);
+
+      // assert
+      // ここではちゃんと変更されている
+      expect(result.name).toEqual(userInput.name);
+    });
+
   });
 });
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -5,11 +5,16 @@ interface UserCreateInput {
   email: string;
 }
 
+interface UserUpdateInput {
+  id: number;
+  name: string;
+}
+
 export class UsersRepository {
   private readonly client: PrismaClient;
 
   constructor(client: PrismaClient) {
-    this.client = client
+    this.client = client;
   }
 
   async getAllUsers(): Promise<User[]> {
@@ -19,6 +24,13 @@ export class UsersRepository {
   async createUser(user: UserCreateInput): Promise<User> {
     return await this.client.user.create({
       data: user,
+    });
+  }
+
+  async updateuser(user: UserUpdateInput): Promise<User> {
+    return await this.client.user.update({
+      where: { id: user.id },
+      data: { name: user.name },
     });
   }
 }
